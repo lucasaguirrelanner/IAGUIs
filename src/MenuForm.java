382b063cc133dcb57228dfb1e3d0b8d208ce4b0d
Lucas Aguirre <lucasaguirrel@Lucass-MacBook-Pro.local>
2025-10-02 08:38:50 -0500

    Initial commit

---
+++ src/MenuForm.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class MenuForm {
+    private JPanel MenuPrincipal;
+    private JButton button1Button;
+    private JButton button2Button;
+    private JButton button_3;
+    private JButton button_4;
+    private JButton Button_exit;
+
+    //this is our constructor!
+    public MenuForm()
+    {
+        setContentPane(MenuPrincipal);
+        setTitle( "Hola");
+        setDefaultClosedOperation( EXIT )
+    }
+}
